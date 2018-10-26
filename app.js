54bd97f85afb73c91c32a9e25b0417946a2d9efa
Son Nguyen <sonnd@dgroup.co>
2018-10-27 02:11:15 +0700

    delay sending msg when sender is typing

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const route = require('./routes')
-
+const colors = require('colors');
 const fs = require("fs");
 const login = require("facebook-chat-api");
 
@@ -20,34 +20,63 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-login({appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8'))}, (err, api) => {
-    if(err) return console.error(err);
-    // Here you can use the api
-    api.listen((err, event) =>{
-      if(err) return console.error(err);
+var threadTimeOutMap = {}
 
-      switch(event.type){
-        case 'message':{
-          console.log(`Received a message: `.green, event.message);
 
-          api.sendMessage('Hello', event.threadID);
+login({ appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8')) }, (err, api) => {
+  if (err) return console.error(err);
+
+  // Here you can use the api
+  api.setOptions({
+    listenEvents: true,
+    logLevel: 'silent'
+  });
+  
+  api.listen((err, event) => {
+    if (err) return console.error(err);
+    
+    switch (event.type) {
+      case 'message': {
+        if (event.isGroup) {
+          return;
         }
-        default:{
-          console.log(event);
-        }
+        console.log(`Received a message: `.green, event);
+        console.log('message threadTimeOutMap'.bgBlue, threadTimeOutMap);
+        sendMessageDebounce(api, event.threadID);
+        break;
       }
-    })
+      case 'typ': {
+        console.log(`someone is  typing: `.green, event);
+        console.log('typ threadTimeOutMap'.bgBlue, threadTimeOutMap);
+        if(event.isTyping && threadTimeOutMap[event.from]){
+          console.log('clear timer to waiting typing end'.red);
+          clearTimeout(threadTimeOutMap[event.from]);
+        }
+        else{
+          console.log('1');
+          if (!event.isTyping){
+            console.log('2');
+            sendMessageDebounce(api, event.from);
+          }
+        }
+        break;
+      }
+      default: {
+        console.log(event);
+      }
+    }
+  })
 });
 
 route(app);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -58,3 +87,26 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
+function sendMessageDebounce(api, threadID) {
+  if (threadTimeOutMap[threadID]) {
+    clearTimeout(threadTimeOutMap[threadID]);
+    console.log('cleared old timer for thread '.bgYellow, threadID);
+  }
+  else{
+    threadTimeOutMap[threadID] = {
+      timer: null,
+      // delayTimer: null,
+    }
+  }
+
+  timeOut = setTimeout(() => {
+    api.sendMessage('Hiện tại mình không thể kiểm tra tin nhắn, vui lòng gọi điện trực tiếp nếu cần liên hệ gấp :)\n\n Đây là tin nhắn tự động', threadID);
+    clearTimeout(threadTimeOutMap[threadID]);
+    delete threadTimeOutMap[threadID];
+  }, 6000)
+
+
+  threadTimeOutMap[threadID] = timeOut
+}
